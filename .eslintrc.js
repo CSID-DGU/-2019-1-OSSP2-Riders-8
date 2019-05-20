51dea6d68fdf7643a51a7819db693067424a3297
giantim <giantim132@gmail.com>
2019-05-20 15:33:25 +0900

    프로젝트 업로드

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'extends': 'airbnb',
+  'rules': {
+    'no-plusplus': 'off',
+    'no-mixed-operators': 'off',
+    'no-bitwise': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  }
+};
